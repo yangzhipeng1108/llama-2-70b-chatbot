734ee117bef14998bc84b675cbe9ab90c9415a21
NLP <33448441+yangzhipeng1108@users.noreply.github.com>
2023-07-27 14:52:06 +0800

    Update example_text_dialogue.py

--- example_text_dialogue.py
+++ example_text_dialogue.py
@@ -1,10 +1,7 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
-
 import fire
 
 from llama import Llama
-
+import os
 
 def main(
     ckpt_dir: str,
@@ -25,21 +22,24 @@ def main(
 
     print("欢迎使用 llama2 模型，输入内容即可进行对话，stop 终止程序")
     while True:
-        query = input("\nuser:")
-        if query.strip() == "stop":
-            break
 
-        prompts = [query]
-        results = generator.text_completion(
-            prompts,
-            max_gen_len=max_gen_len,
-            temperature=temperature,
-            top_p=top_p,
-        )
-        for prompt, result in zip(prompts, results):
-            print(prompt)
-            print(f"> {result['generation']}")
-            print("\n==================================\n")
+        local_rank = int(os.environ["LOCAL_RANK"])
+        if local_rank == 0:
+            query = input("\nuser:")
+            if query.strip() == "stop":
+                break
+    
+            prompts = [query]
+            results = generator.text_completion(
+                prompts,
+                max_gen_len=max_gen_len,
+                temperature=temperature,
+                top_p=top_p,
+            )
+            for prompt, result in zip(prompts, results):
+                print(prompt)
+                print(f"> {result['generation']}")
+                print("\n==================================\n")
 
 
 if __name__ == "__main__":
