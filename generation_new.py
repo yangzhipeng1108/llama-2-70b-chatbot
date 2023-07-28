8d9ac94e844de953cd5b55594bcf4f228c497808
NLP <33448441+yangzhipeng1108@users.noreply.github.com>
2023-07-28 10:28:10 +0800

    Update generation_new.py

--- generation_new.py
+++ generation_new.py
@@ -1,12 +1,33 @@
-
+import json
+import os
+import sys
+import time
+from pathlib import Path
 from typing import List, Literal, Optional, Tuple, TypedDict
+
 import torch
 import torch.nn.functional as F
+
+from fairscale.nn.model_parallel.initialize import (
+    get_model_parallel_rank,
+    initialize_model_parallel,
+    model_parallel_is_initialized,
+)
+
+from llama.model import ModelArgs, Transformer
+from llama.tokenizer import Tokenizer
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+import torch.distributed as dist
+
+Role = Literal["system", "user", "assistant"]
+
 
 class CompletionPrediction(TypedDict, total=False):
     generation: str
     tokens: List[str]  # not required
     logprobs: List[float]  # not required
+
 
 def sample_top_p(probs, p):
     probs_sort, probs_idx = torch.sort(probs, dim=-1, descending=True)
@@ -19,15 +40,62 @@ def sample_top_p(probs, p):
     return next_token
 
 
+def build(
+        ckpt_dir: str,
+        tokenizer_path: str,
+        max_seq_len: int,
+        max_batch_size: int,
+        model_parallel_size: Optional[int] = None,
+) -> "Llama":
+    global_rank = int(os.environ["RANK"])
+    local_rank = int(os.environ["LOCAL_RANK"])
+    torch.cuda.set_device(global_rank % torch.cuda.device_count())
+    dist.init_process_group(backend="nccl")
+
+    torch.cuda.set_device(local_rank)
+    device = torch.device("cuda", local_rank)
+
+    torch.distributed.barrier()
+
+    start_time = time.time()
+    checkpoints = sorted(Path(ckpt_dir).glob("*.pth"))
+    assert len(checkpoints) > 0, f"no checkpoint files found in {ckpt_dir}"
+    assert model_parallel_size == len(
+        checkpoints
+    ), f"Loading a checkpoint for MP={len(checkpoints)} but world size is {model_parallel_size}"
+    ckpt_path = checkpoints[get_model_parallel_rank()]
+    checkpoint = torch.load(ckpt_path, map_location="cpu")
+    with open(Path(ckpt_dir) / "params.json", "r") as f:
+        params = json.loads(f.read())
+
+    model_args: ModelArgs = ModelArgs(
+        max_seq_len=max_seq_len,
+        max_batch_size=max_batch_size,
+        **params,
+    )
+    tokenizer = Tokenizer(model_path=tokenizer_path)
+    model_args.vocab_size = tokenizer.n_words
+    torch.set_default_tensor_type(torch.cuda.HalfTensor)
+    model = Transformer(model_args)
+    model.load_state_dict(checkpoint, strict=False)
+    print(f"Loaded in {time.time() - start_time:.2f} seconds")
+
+    model = model.to(device)
+    # DistributedDataParallel
+    model = DDP(model, device_ids=[local_rank], output_device=local_rank)
+
+    return model, tokenizer
+
+
 @torch.inference_mode()
 def generate(
-    model,tokenizer,
-    prompt_tokens: List[List[int]],
-    max_gen_len: int,
-    temperature: float = 0.6,
-    top_p: float = 0.9,
-    logprobs: bool = False,
-    echo: bool = False,
+        model, tokenizer,
+        prompt_tokens: List[List[int]],
+        max_gen_len: int,
+        temperature: float = 0.6,
+        top_p: float = 0.9,
+        logprobs: bool = False,
+        echo: bool = False,
 ) -> Tuple[List[List[int]], Optional[List[List[float]]]]:
     params = model.params
     bsz = len(prompt_tokens)
@@ -51,9 +119,9 @@ def generate(
     for cur_pos in range(min_prompt_len, total_len):
         logits = model.forward(tokens[:, prev_pos:cur_pos], prev_pos)
         if logprobs:
-            token_logprobs[:, prev_pos + 1 : cur_pos + 1] = -F.cross_entropy(
+            token_logprobs[:, prev_pos + 1: cur_pos + 1] = -F.cross_entropy(
                 input=logits.transpose(1, 2),
-                target=tokens[:, prev_pos + 1 : cur_pos + 1],
+                target=tokens[:, prev_pos + 1: cur_pos + 1],
                 reduction="none",
                 ignore_index=pad_id,
             )
@@ -70,7 +138,7 @@ def generate(
         )
         tokens[:, cur_pos] = next_token
         eos_reached |= (~input_text_mask[:, cur_pos]) & (
-            next_token == tokenizer.eos_id
+                next_token == tokenizer.eos_id
         )
         prev_pos = cur_pos
         if all(eos_reached):
@@ -82,10 +150,10 @@ def generate(
     for i, toks in enumerate(tokens.tolist()):
         # cut to max gen len
         start = 0 if echo else len(prompt_tokens[i])
-        toks = toks[start : len(prompt_tokens[i]) + max_gen_len]
+        toks = toks[start: len(prompt_tokens[i]) + max_gen_len]
         probs = None
         if logprobs:
-            probs = token_logprobs[i][start : len(prompt_tokens[i]) + max_gen_len]
+            probs = token_logprobs[i][start: len(prompt_tokens[i]) + max_gen_len]
         # cut to eos tok if any
         if tokenizer.eos_id in toks:
             eos_idx = toks.index(tokenizer.eos_id)
@@ -94,9 +162,10 @@ def generate(
         out_tokens.append(toks)
         out_logprobs.append(probs)
     return (out_tokens, out_logprobs if logprobs else None)
-    
+
+
 def text_completion(
-        model,tokenizer,
+        model, tokenizer,
         prompts: List[str],
         temperature: float = 0.6,
         top_p: float = 0.9,
@@ -108,7 +177,7 @@ def text_completion(
         max_gen_len = model.params.max_seq_len - 1
     prompt_tokens = [tokenizer.encode(x, bos=True, eos=False) for x in prompts]
     generation_tokens, generation_logprobs = generate(
-        model,tokenizer,
+        model, tokenizer,
         prompt_tokens=prompt_tokens,
         max_gen_len=max_gen_len,
         temperature=temperature,
